4b6ac0adbc96cdb492d9988490480b9d6f4b3412
ddaanii <torxdani1@gmail.com>
2025-01-21 14:40:54 +0100

    Created and implemented Wishlist.cs

---
+++ CatAPILib/Models/Wishlist.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatAPILib.Models
+{
+    public class Wishlist
+    {
+        required public Cat[] wishlist {  get; set; }
+        required public int page { get; set; }
+        required public string viewing { get; set; }
+        required public int totalItems { get; set; }
+
+    }
+}
