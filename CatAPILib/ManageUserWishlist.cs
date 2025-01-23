12ecd253de61ec5808113a9e388276ccf34a8d40
ddaanii <torxdani1@gmail.com>
2025-01-23 08:59:52 +0100

    Cherry picked modification from UIDEnpoints

---
+++ CatAPILib/ManageUserWishlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatAPILib
+{
+    public class ManageUserWishlist
+    {
+        public static async GetWishlist(string uid)
+        {
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, URIBuilder.BuildDomain($"/users/{uid}/wishlist"));
+            }
+            catch(Exception ex)
+            {
+
+            }
+    }
+}
