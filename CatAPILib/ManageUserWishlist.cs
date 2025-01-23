15af9c0bd7eeb1579c7fb5c9c8ca9bb941b3898e
ddaanii <torxdani1@gmail.com>
2025-01-23 19:24:03 +0100

    Completed User Endpoints Implementation

---
+++ CatAPILib/ManageUserWishlist.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading.Tasks.Dataflow;
+using CatAPILib.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace CatAPILib
+{
+    public class ManageUserWishlist
+    {
+        public static async Task<Wishlist> GetWishlist(string uid)
+        {
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, URIBuilder.BuildDomain($"/users/{uid}/wishlist"));
+                var response = await client.SendAsync(request);
+                response.EnsureSuccessStatusCode();
+
+                var jsonString = await response.Content.ReadAsStringAsync();
+
+                
+                Wishlist ret = JsonConvert.DeserializeObject<Wishlist>(jsonString)!;
+               
+
+                return ret;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new Wishlist([], 0, "", 0);
+            }
+        }
+
+        /*public static async Task<> UpdateWishlist() { 
+        }*/
+    }
+}
---
+++ CatAPILib/Models/User.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatAPILib.Models
+{
+    public class User
+    {
+        [System.Diagnostics.CodeAnalysis.SetsRequiredMembersAttribute]
+        public User(string email, string username, string password, string emailConfirmationBaseUrl, string description, string pronouns)
+        {
+            this.email = email;
+            this.username = username;
+            this.password = password;
+            this.emailConfirmationBaseUrl = emailConfirmationBaseUrl;
+            this.description = description;
+            this.pronouns = pronouns;
+        }
+
+        required public string email {  get; set; }
+        required public string username { get; set; }
+        required public string password { get; set; }
+        required public string emailConfirmationBaseUrl { get; set; }
+        required public string description { get; set; }
+        required public string pronouns { get; set; }
+    }
+}
