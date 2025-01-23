901a9026e8b929cb6db1e7557cc8955e797dfa83
ddaanii <torxdani1@gmail.com>
2025-01-23 08:50:11 +0100

    Implemented URIBuilder class to build URI for API calls

---
+++ CatAPILib/URIBuilder.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CatAPILib
+{
+    internal class URIBuilder
+    {
+        public static string domain = "https://kittens.robaldo.dev/api/v1";
+
+        public static string BuildDomain(string path)
+        {
+            return domain + path;
+        }
+    }
+}
