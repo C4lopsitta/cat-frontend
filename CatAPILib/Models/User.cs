3562b75ebb9e535d9c901cdffe5883a98df20c8a
ddaanii <torxdani1@gmail.com>
2025-01-28 18:23:47 +0100

    Implemented GetAllUser method in UserEnpoints and fixed bugs

--- CatAPILib/Models/User.cs
+++ CatAPILib/Models/User.cs
@@ -2,13 +2,15 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.Json.Serialization;
 using System.Threading.Tasks;
 
 namespace CatAPILib.Models
 {
     public class User
     {
-        [System.Diagnostics.CodeAnalysis.SetsRequiredMembersAttribute]
+
+        [System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
         public User(string email, string username, string password, string emailConfirmationBaseUrl, string description, string pronouns)
         {
             this.email = email;
@@ -19,11 +21,36 @@ namespace CatAPILib.Models
             this.pronouns = pronouns;
         }
 
-        required public string email {  get; set; }
-        required public string username { get; set; }
-        required public string password { get; set; }
-        required public string emailConfirmationBaseUrl { get; set; }
-        required public string description { get; set; }
-        required public string pronouns { get; set; }
+        [JsonConstructor]
+        public User(string? uid, string username, string? image, string? imageMime, string description, string pronouns, string[]? cats, string[]? wishlist)
+        {
+            this.uid = uid;
+            this.username = username;
+            this.image = image;
+            this.imageMime = imageMime;
+            this.description = description ?? "";
+            this.pronouns = pronouns ?? "";
+            this.cats = cats;
+            this.wishlist = wishlist;
+        }
+
+        [System.Diagnostics.CodeAnalysis.SetsRequiredMembers]
+        public User()
+        {
+        }
+
+        public string? uid { get; set; }
+        // login
+        public string? email {  get; set; }
+        public string? username { get; set; }
+        public string? password { get; set; }
+        public string? emailConfirmationBaseUrl { get; set; }
+        public string? description { get; set; }
+        public string? pronouns { get; set; }
+        // ----
+        public string? image { get; set; }
+        public string? imageMime { get; set; }
+        public string[]? cats { get; set; }
+        public string[]? wishlist { get; set; }
     }
 }
--- CatAPILib/UserEndpoints.cs
+++ CatAPILib/UserEndpoints.cs
@@ -11,7 +11,33 @@ namespace CatAPILib
 {
     public class UserEndpoints
     {
-        public static async Task<Dictionary<string, string>>? RegisterUser(User user)
+        public static async Task<List<User>?> GetAllUsers()
+        {
+            var jsonString = "";
+
+            try
+            {
+                var client = new HttpClient();
+                var request = new HttpRequestMessage(HttpMethod.Get, URIBuilder.BuildDomain("/users"));
+
+                var response = await client.SendAsync(request);
+                jsonString = await response.Content.ReadAsStringAsync();
+
+                response.EnsureSuccessStatusCode();
+
+                var ret = JsonConvert.DeserializeObject<List<User>>(jsonString);
+
+                return ret!;
+            }
+            catch (HttpRequestException e)
+            {
+                Console.WriteLine(e.Message + jsonString);
+                return null;
+                
+            }
+        }
+
+        public static async Task<Dictionary<string, dynamic>?> RegisterUser(User user)
         {
             var jsonString = "";
 
@@ -33,19 +59,19 @@ namespace CatAPILib
 
                 response.EnsureSuccessStatusCode();
 
-                var ret = JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString);
+                var ret = JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonString);
 
                 return ret!;
             }
             catch (HttpRequestException e)
             {
-                Console.WriteLine(e.Message);
-                return JsonConvert.DeserializeObject<Dictionary<string, string>>(jsonString)!;
+                Console.WriteLine(e.Message + jsonString);
+                return null;
             }
         }
 
 
-        public static async Task<Dictionary<string, dynamic>> ValidateNewUserAccount(string uid, string confirmationId)
+        public static async Task<Dictionary<string, dynamic>?> ValidateNewUserAccount(string uid, string confirmationId)
         {
 
             var jsonString = "";
@@ -71,13 +97,13 @@ namespace CatAPILib
             {
                 {
                     Console.WriteLine(e.Message);
-                    return JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonString)!;
+                    return null;
                 }
             }
         }
     
 
-    public static async Task<Dictionary<string, dynamic>> AuthenticateUser(string email, string password, int? tfa = null)
+    public static async Task<Dictionary<string, dynamic>?> AuthenticateUser(string email, string password, int? tfa = null)
         {
             {
 
@@ -109,7 +135,7 @@ namespace CatAPILib
                 {
                     {
                         Console.WriteLine(e.Message);
-                        return JsonConvert.DeserializeObject<Dictionary<string, dynamic>>(jsonString)!;
+                        return null;
                     }
                 }
             }
