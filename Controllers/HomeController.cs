d09a9e9bb3e27705e254a870ab21bccc17421b0e
ddaanii <torxdani1@gmail.com>
2025-01-28 18:29:50 +0100

    Cleaned Controllers and Views from test code

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,44 +16,6 @@ namespace cat_frontend.Controllers
 
         public IActionResult Index()
         {
-            //var signIn = UserEndpoints.RegisterUser(new CatAPILib.Models.User(
-            //        "torxdani1+test03@gmail.com",
-            //        "dani123123",
-            //        "!!CiaLALAL1212",
-            //        "localhost:8000/user/confirm/",
-            //        "no.",
-            //        "la/a"
-            //    )).Result;
-
-            //string confirmId = "825c73f4b1bd406ea32e8ef5e3489049035947839b77b56ff588652a";
-            //var confirm = UserEndpoints.ValidateNewUserAccount(signIn!["uid"], confirmId);
-            var responseVal = UserEndpoints.GetAllUsers().Result;
-
-            if(responseVal == null)
-            {
-                ViewData["errore"] = "AAAAAAAAAAAAAAAAAAAAAAAAAA";
-            }
-            else
-            {
-                foreach(CatAPILib.Models.User user in responseVal)
-                {
-                    ViewData["uid"] += user.uid+ " ";
-                    ViewData["username"] += user.username + " ";
-                    ViewData["image"] += user.image + " ";
-                    ViewData["imageMime"] += user.imageMime + " ";
-                    ViewData["description"] += user.description + " ";
-                    ViewData["pronouns"] += user.pronouns + " ";
-
-                    foreach(string cat in user.cats!)
-                    {
-                        ViewData["cats"] += cat;
-                    }
-
-                }
-
-
-            }
-
             return View();
         }
 
